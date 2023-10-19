74c39be9ba13f83e52649d191258c09f273ca8a6
piyawat pintusornsri <piyawat@softever.co.th>
2023-10-19 14:20:51 +0700

    express tutorial

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("dog");
+const express = require("express");
+const app = express();
+
+app.get("/say", function (req, res) {
+  res.send("Hello World");
+});
+
+app.get("/user/:id", function (req, res) {
+  res.send(`hello ${req.params.name}`);
+});
+
+app.get("/search", function (req, res) {
+  res.send(`search: ${req.query.name} ${req.query.id}`);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+  console.log("Press ctrl + c to quit.");
+});
