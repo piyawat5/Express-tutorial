928458c1ade050be899cdb7f8ffbec6e8f1c64cd
piyawat pintusornsri <piyawat5930302600@gmail.com>
2023-10-20 11:15:26 +0700

    add middleware, put, post, delete products api

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+//Middlewares
+app.use(express.json());
+
 class Product {
   constructor(id, name, image, price, stock) {
     this.id = id;
@@ -11,12 +14,10 @@ class Product {
   }
 }
 
-const products = [
+let products = [
   new Product(1, "Macbook Pro", "", 90000, 0),
   new Product(2, "iPhone XS", "", 50000, 10),
 ];
-
-app.get("/say", (req, res) => {});
 
 app.get("/products", (req, res) => {
   res.json(products);
@@ -63,6 +64,38 @@ app.get("/products/price", (req, res) => {
 app.get("/products/:id", (req, res) => {
   const result = products.filter((product) => product.id == req.params.id);
   result.length > 0 ? res.json(result[0]) : res.status(404).json(null);
+});
+
+app.post("/products", (req, res) => {
+  const { name, price, stock } = req.body;
+  let product = new Product(products.length + 1, name, "", price, stock);
+
+  products = [...products, product];
+
+  res.status(201).json(product);
+});
+
+app.put("/products/:id", (req, res) => {
+  const { name, price, stock } = req.body;
+  let findIndex = products.findIndex((product) => product.id == req.params.id);
+  if (findIndex !== -1) {
+    products[findIndex] = { ...products[findIndex], name, price, stock };
+    res.json(products[findIndex]);
+  } else {
+    res.status(404).json(null);
+  }
+});
+
+app.delete("/products/:id", (req, res) => {
+  const id = req.params.id;
+  const findIndex = products.findIndex((product) => product.id == id);
+
+  if (findIndex !== -1) {
+    products.splice(findIndex, 1);
+    res.status(204).json({ id });
+  } else {
+    res.status(404).json({ id });
+  }
 });
 
 const port = 3000;
