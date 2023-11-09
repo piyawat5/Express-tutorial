b74f8edeeaa20ad8c5fe6a369d6d28993b668168
piyawat pintusornsri <piyawat5930302600@gmail.com>
2023-11-09 17:26:11 +0700

    remove verifyToken temporary

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const jwt = require("../configs/jwt");
 
-router.use("/products", jwt.verifyToken, require("./product.route"));
+router.use("/products", require("./product.route"));
 router.use("/account", require("./account.route"));
 router.use("/user", require("./user.route"));
 
