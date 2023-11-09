0907f74feb12ed6093e1ace0e75c4c40f2cb1545
piyawat pintusornsri <piyawat5930302600@gmail.com>
2023-11-09 17:46:55 +0700

    fix CORS to be all request

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ app.use(express.json());
 app.use("/image", express.static("./images"));
 
 //CORS
-const corsOptions = {
-  origin: [
-    "http://example.com",
-    "http://localhost:3000",
-    "https://www.w3schools.com",
-  ],
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: [
+//     "http://example.com",
+//     "http://localhost:3000",
+//     "https://www.w3schools.com",
+//   ],
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
+app.use(cors());
 
 //Routes api
 app.use(require("./src/routes/routes"));
